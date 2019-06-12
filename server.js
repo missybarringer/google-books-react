043260f143e178e49a0f0de323dc414a5fd45e95
GoatFeatherFarm <44263291+missybarringer@users.noreply.github.com>
2019-06-12 11:05:29 -0500

    added code comments

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+// heroku can use any port it wants else we use port 3001
 const PORT = process.env.PORT || 3001;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets
+// Serve up static assets (usually on heroku) for the production environment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -16,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-// Connect to the Mongo DB
+// Connect to the Mongo DB or mlab on heroku
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://user:Password123@ds259154.mlab.com:59154/heroku_tmnvmhc0",
   {
